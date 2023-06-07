655462f381d98e16f72e2d6602ccc9813463d6f6
alcantaraedw <135863438+alcantaraedw@users.noreply.github.com>
2023-06-07 14:25:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, este é meu projeto");
+            Console.WriteLine("Ele é um teste para o GitHub");
+            Console.WriteLine("Nada de novo por aqui...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
